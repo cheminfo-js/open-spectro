0e677fbf4cc62cfbf029cf9d3806f984b7ef3632
Luc Patiny <luc@patiny.com>
2016-03-15 20:43:49 +0100

    Add test case

---
+++ test/parse.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+var FS = require('fs');
+var OSP = require('..');
+
+describe.only('test parse', function () {
+    it('should parse coorectly the txt file', function() {
+        var options= {
+            smooth: 8,
+            normalize: true,
+            type:'T'
+        };
+
+        var dataString=FS.readFileSync('./data/exp.txt', 'utf8');
+        var result=OSP.parse(dataString);
+        (Object.keys(result).length).should.equal(4);
+        var spectraArray=OSP.Util.toArray(result);
+        spectraArray.length.should.equal(4);
+    });
+
+});
